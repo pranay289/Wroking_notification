0b3923ab771a744189bd62d9517d36b1ab5cb483
Pranay Bhaturkar <47419296+pranay289@users.noreply.github.com>
2020-05-28 21:40:17 +0530

    Add files via upload

---
+++ app/src/main/java/com/pranay/mynotification/Apiclient.java
@@ -0,0 +1,37 @@
+package com.pranay.mynotification;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Apiclient {
+
+    private static Retrofit retrofit=null;
+    private static Apiclient retrofitclient;
+    public  Apiclient()
+    {
+
+        if (retrofit==null)
+        {
+            retrofit=new Retrofit.Builder()
+                    .baseUrl("https://www.webrooper.com/androiddb/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+
+    }
+
+    public static synchronized Apiclient getInstance()
+    {
+        if (retrofitclient==null)
+        {
+            retrofitclient=new Apiclient();
+        }
+        return retrofitclient;
+    }
+
+    public Api getapi()
+    {
+        return retrofit.create(Api.class);
+    }
+}
