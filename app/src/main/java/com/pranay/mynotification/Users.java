7fdcde93996c9fe78613184e7cf926c4a5557652
Pranay Bhaturkar <47419296+pranay289@users.noreply.github.com>
2020-05-28 21:41:45 +0530

    Add files via upload

---
+++ app/src/main/java/com/pranay/mynotification/Users.java
@@ -0,0 +1,14 @@
+package com.pranay.mynotification;
+
+public class Users {
+
+    boolean response;
+
+    public boolean getResponse(){
+        return response;
+    }
+
+    public void setResponse(boolean response) {
+        this.response = response;
+    }
+}
